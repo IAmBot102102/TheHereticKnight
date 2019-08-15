232aa9d619f1c4e00a211a14bb87f42024c93c49
IAmBot102102 <BotNumber102102@gmail.com>
2019-08-15 13:36:23 -0400

    Added Direction to the Player

--- Scripts/PlayerStatsAndMovements.gd
+++ Scripts/PlayerStatsAndMovements.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 #Player Object Class
 const SPEED = 200
 #Speed of movement
+var Direction = "Down"
+#Direction the Charater is Facing
 var movedir = Vector2(0,0)
 #MoveDirection is the vector2dthingsomeoneelsedothispls
 onready var PlayerStats = {
@@ -12,7 +14,8 @@ onready var PlayerStats = {
 func _physics_process(delta):
 	controls_loop()
 	movement_loop()
-	
+	AnimationLoop()
+	print(Direction)
 func controls_loop():
 	var LEFT = Input.is_action_pressed("ui_left")
 	var RIGHT = Input.is_action_pressed("ui_right")
@@ -25,3 +28,14 @@ func movement_loop():
 	var motion = movedir.normalized() * SPEED
 	move_and_slide(motion, Vector2(0,0))
 	#move_and_slide didnt work because i was adding the speed to the normalized movedirection
+func AnimationLoop():
+	#Loop to match the MovingDirection to the AnimtionFrame
+	match movedir:
+		Vector2(-1,0):
+			Direction = "Left"
+		Vector2(1,0):
+			Direction = "Right"
+		Vector2(0, -1):
+			Direction = "Up"
+		Vector2(0,1):
+			Direction = "Down"
