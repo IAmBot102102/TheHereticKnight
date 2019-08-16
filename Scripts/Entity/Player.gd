d6ae36860392f51e6db0556e904373cb63a930eb
IAmBot102102 <BotNumber102102@gmail.com>
2019-08-16 18:48:51 -0400

    Changed Icon

--- Scripts/Entity/Player.gd
+++ Scripts/Entity/Player.gd
@@ -4,8 +4,9 @@ extends Entity
 The main player script contains Input, Movement and
 various other things for the user to control.
 """
-
+var DirectionFaced = "RIGHT"
 var movedir := Vector2()
+var collisionWithEnemy = false
 func _ready():
 	add_to_group("playable", true)
 	add_to_group("trackable", true)
@@ -15,6 +16,7 @@ func _on_collision(body : KinematicCollision2D):
 	if body.collider is Entity:
 		if body.collider.is_in_group("enemy"):
 			Health -= 20
+			collisionWithEnemy = true
 
 func _input(event):
 	if event is InputEventKey:
@@ -24,6 +26,19 @@ func _input(event):
 		var DOWN = Input.is_action_pressed("ui_down")
 		movedir.x = -int(LEFT) + int(RIGHT)
 		movedir.y = -int(UP) + int(DOWN)
+		if LEFT == true:
+			DirectionFaced = "LEFT"
+		if RIGHT == true:
+			DirectionFaced = "RIGHT"
+		if UP == true:
+			DirectionFaced = "UP"
+		if DOWN == true:
+			DirectionFaced = "DOWN"
+		
 
 func movement(delta) -> Vector2:
-	return movedir.normalized() * Speed * delta
+	if collisionWithEnemy == false:
+		return movedir.normalized() * Speed * delta
+	else:
+		collisionWithEnemy = false
+		return movedir.normalized() * Speed * delta * -10
