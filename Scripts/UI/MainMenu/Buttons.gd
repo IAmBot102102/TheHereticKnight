6cc7c3ca31bd5d302f230806a603c10a3133a1af
Kyle Guarco <kyleguarco55@gmail.com>
2019-08-20 08:45:42 -0400

    Added a new quit button to the menu

--- Scripts/UI/MainMenu/Buttons.gd
+++ Scripts/UI/MainMenu/Buttons.gd
@@ -2,10 +2,12 @@ extends VBoxContainer
 
 onready var Play = $Play
 onready var Options = $Options
+onready var Quit = $Quit
 
 func _ready():
 	Play.connect("pressed", self, "_on_play_press")
 	Options.connect("pressed", self, "_on_options_press")
+	Quit.connect("pressed", self, "_on_quit_press")
 
 func _on_play_press():
 	# Gets the setting for this scene. It's located under
@@ -23,3 +25,6 @@ func _on_options_press():
 	get_parent().move_child(options_scene, 1)
 	
 	self.queue_free()
+
+func _on_quit_press():
+	get_tree().quit()
