78d43e5bc2f576aa570c514f74f06f7a1d41e29c
IAmBot102102 <53452898+IAmBot102102@users.noreply.github.com>
2021-01-04 22:35:49 -0500

    Added basic movement and some sad assets

---
+++ HereticKnight/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxVelocity;
+
+    public Vector3 currentVelocity = new Vector3(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Something");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            currentVelocity += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            currentVelocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            currentVelocity += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            currentVelocity += new Vector3(1, 0, 0);
+        }
+        currentVelocity *= (maxVelocity/Mathf.Pow(2,6));
+
+        this.gameObject.transform.position += currentVelocity;
+
+
+    }
+}
